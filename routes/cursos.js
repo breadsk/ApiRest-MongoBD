3c4a24238fa894237685b3d27fc7b41706cc6fe8
Nicolas <nicolas.programador@gmail.com>
2026-01-22 17:07:45 -0300

    modelo de curso creado

--- routes/cursos.js
+++ routes/cursos.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const ruta = express.Router();
+const Curso = require('../models/curso_model');
+const Joi = require('joi');
 
 ruta.get('/', (req, res) => {
     res.json('Listo el GET de cursos');
